a5d3e985814064f597f63c6a0eeff2b18daf8da2
jcas96 <162757478+jcas96@users.noreply.github.com>
2024-11-02 15:51:03 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ export async function redirectToAuthCodeFlow(clientId){
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri","http://localhost:5173/callback");
+    params.append("redirect_uri","https://top-charts-9caa721698e8.herokuapp.com/callback");
     params.append("scope","user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -66,7 +66,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://top-charts-9caa721698e8.herokuapp.com/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
